64cda02ad09c322c4602377f7087156f694701d3
fayeariwaodo <fayeabache@gmail.com>
2022-05-03 16:18:52 +0100

    first commit

--- Portfolio.js
+++ Portfolio.js
@@ -15,10 +15,10 @@ $(document).ready(function () {
   });
 
   // slide up script
-  $(".scroll-up-btn").click(function(){
-    $('html').animate({scrollTop: 0});
+  $(".scroll-up-bttn").click(function(){
+     $('html').animate({scrollTop: 0});
     // removing smooth scroll on slide-up button click
-    $('html').css("scrollBehavior", "auto");
+      $('html').css("scrollBehavior", "auto");
   });
 
   // toggle menu/navbar Script
