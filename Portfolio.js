4ea7e637336d491c2407a662ad58e2d13ea05e08
fayeariwaodo <fayeabache@gmail.com>
2022-05-21 19:49:02 +0100

    first commit

--- Portfolio.js
+++ Portfolio.js
@@ -5,21 +5,21 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("sticky");
     }
-    if(this.scrollY > 500){
-      $(".scroll-up-btn").addClass("show");
+    // if(this.scrollY > 500){
+    //   $(".scroll-up-btn").addClass("show");
 
-    }else{
-      $(".scroll-up-btn").removeClass("show");
+    // }else{
+    //   $(".scroll-up-btn").removeClass("show");
 
-    }
+    // }
   });
 
   // slide up script
-  $(".scroll-up-bttn").click(function(){
-     $('html').animate({scrollTop: 0});
+  // $(".scroll-up-bttn").click(function(){
+  //    $('html').animate({scrollTop: 0});
     // removing smooth scroll on slide-up button click
-      $('html').css("scrollBehavior", "auto");
-  });
+  //     $('html').css("scrollBehavior", "auto");
+  // });
 
   // toggle menu/navbar Script
   $(".menu-btn").click(function () {
